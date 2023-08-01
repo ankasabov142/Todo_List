516103ea4143d4a758b267c4439a200395257b16
Angel Kasabov <ankasabov142@gmail.com>
2023-08-01 14:39:01 +0300

    Updated code comments

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 $(document).ready(function () {
+  //Recieve main div container and main addTask button.
   const mainButton = document.querySelector(".buttonAdd");
   const taskList = document.querySelector(".taskList");
+  //addTask button functionality.
   $(mainButton).on("click", function (e) {
     $(taskList).append(`<div class="task ">
     <div class="liHeader">
@@ -19,10 +21,13 @@ $(document).ready(function () {
     </div>
     
   </div>`);
+    //Giving each task list its own identification through a time stamp placed in the id of the list.
     let currTime = e.timeStamp;
     let currentTask = taskList.lastChild;
     $(currentTask).attr("id", `${currTime}`);
+    //Pass functionality to each button attached to the individual task list.
     $(`#${currTime} .addUnit`).on("click", function (e) {
+      //AddTask Unit button functionality
       $(`#${currTime}`).append(`<div class="taskUnit">
       <div class="left">
       <div class="checkbox">
@@ -42,6 +47,7 @@ $(document).ready(function () {
           .parent()
           .parent()
           .siblings(".taskTextWrapper")[0];
+        //checked button, line-through style
         if (e.currentTarget.checked) {
           $(textEl).css({
             "text-decoration": "line-through",
@@ -51,26 +57,28 @@ $(document).ready(function () {
         } else {
           $(textEl).css("text-decoration", "none");
         }
-
       });
+      //Delete functionality by the "Delete button" inside of every task unit
       $(`.taskInsideButton`).on("click", function (e) {
         let parent = $(e.currentTarget).parent();
-
         parent.remove();
       });
+      //Delete functionality by the "Delete button" inside of every task list
     });
     $(`#${currTime} .deleteUnit`).on("click", function (e) {
       $(`#${currTime}`).remove();
     });
+    //Add containers to save buttons, while the task list is in Edit mode
     const butDiv = $(`#${currTime} .butDiv`);
     const editBtn = $(`#${currTime} .editUnit`);
-
+    //Add 'tick' button , to save the changes while Edit mode
     const doneEditBtn = $(document.createElement("div"))
       .html(`<button class="headerButtons finishAll">
 <span class="material-symbols-outlined">
 done_all
 </span>
 </button>`);
+    //Assigning functionality to the "Edit Button", by creating separate containers in which the information previously set is saved.
 
     editBtn.on("click", function editFunc(e) {
       let taskValues = [];
@@ -85,7 +93,7 @@ done_all
           `<input type="text" class='taskEditInput' value="${text}">`
         );
       }
-
+      // Тhrough this method the application manages to keep the unchanged elements and save the changes made during the "Edit Mode"
       const headingWrapperElement = $(`#${currTime} .liHeader`);
 
       const headingElement = $(`#${currTime} .headingText`)[0];
@@ -95,7 +103,7 @@ done_all
       $(headingWrapperElement).prepend(
         `<input type="text" value="${headingText}">`
       );
-
+      //Replacing edit button while "Edit mode" is on
       editBtn.remove();
       $(butDiv).prepend(doneEditBtn);
 
@@ -118,6 +126,7 @@ done_all
         headingTextInput.remove();
 
         $(headingWrapperElement).prepend(headingElement);
+        //Adding edit button while "Edit mode" is done
 
         doneEditBtn.remove();
 
